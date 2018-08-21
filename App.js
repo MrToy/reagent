e2c445667793a601b29e3ab7748b4a9a45a543cb
Toy <1659808224@qq.com>
2018-08-21 17:39:08 +0800

    ios集成启动图

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import Routes from './src/routes';
-
+import SplashScreen from 'react-native-splash-screen'
 // console.disableYellowBox = true;
 export default class App extends Component {
+  componentDidMount() {
+    SplashScreen.hide()
+  }
   render() {
     return (
       <Routes />
