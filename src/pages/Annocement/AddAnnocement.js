8f54e60b1a5c324997b7515ad2d704db4ab2ce59
Toy <1659808224@qq.com>
2018-08-23 09:43:20 +0800

    发布通告页

--- src/pages/Annocement/AddAnnocement.js
+++ src/pages/Annocement/AddAnnocement.js
@@ -1,17 +1,31 @@
 import React from 'react';
-import { View ,ScrollView} from 'react-native';
-import FormButton from '../../lib/FormButton';
-import FormInput from '../../lib/FormInput';
+import { Alert, Button, ScrollView, TextInput,Text } from 'react-native';
+import { scale } from '../../utils/dimension';
 
 
 export default class extends React.Component {
     static navigationOptions = {
-        title: '发布实验室通告'
+        title: '发布实验室通告',
+        headerRight: (
+            <Button
+                onPress={() => Alert.alert('后面保存成功的～')}
+                title="保存"
+                color="#fff"
+            />
+        )
     };
     render() {
         return (
-            <ScrollView style={{ padding: 20,backgroundColor:"#fff"}}>
-
+            <ScrollView style={{ padding: 30,backgroundColor:"#FCF8F8"}}>
+                <Text style={{fontSize:scale(20)}}>标题</Text>
+                <TextInput
+                    autoCapitalize="none"
+                    style={{borderColor:"#555",borderWidth:1,marginTop:scale(10),marginBottom:scale(25),lineHeight:scale(30),height:scale(30),paddingHorizontal:scale(10),backgroundColor:"#fff"}} />
+                <Text style={{fontSize:scale(20)}}>内容</Text>
+                <TextInput
+                    autoCapitalize="none"
+                    style={{borderColor:"#555",borderWidth:1,marginTop:scale(10),marginBottom:scale(25),lineHeight:scale(30),height:scale(270),paddingHorizontal:scale(10),backgroundColor:"#fff"}}
+                    multiline={true} />
             </ScrollView>
         )
     }
